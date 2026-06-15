3819284dc85e3abc017832696499241bf52d6961
iliberato dev <isaque.cedesp@gmail.com>
2026-06-15 14:26:46 -0300

    Make first Test

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,10 @@
-const calculaComissaoVenda = require('.');
+const calculaComissaoVenda = require('./index');
 
+it('Se a lista estiver vazia, a comissão é zero', () => {
 
+    const resultadoAtual = calculaComissaoVenda([]);
+    const resultadoEsperado = 0;
+
+    expect(resultadoAtual).toBe(resultadoEsperado);
+})
 
