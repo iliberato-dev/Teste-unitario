61501b24e8c08e6910055d8a08591c158a613c7d
iliberato dev <isaque.cedesp@gmail.com>
2026-06-15 15:12:56 -0300

    Add test 3

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 module.exports = function (items) {
-    
-    if (!items.length) return 0;
+  if (!items.length) return 0;
 
-    const { precoUnitario, qtdVendida } = items[0]
+  if (items.length === 1) {
+    const { precoUnitario, qtdVendida } = items[0];
 
-    return precoUnitario * qtdVendida * .05
-}
+    return precoUnitario * qtdVendida * 0.05;
+  }
+
+  if (items.length > 1) {
+    const prices = items.map(item => {
+        const { precoUnitario, qtdVendida } = item
+        return precoUnitario * qtdVendida
+    })
+
+    const totalPrice = prices.reduce((intemAnterior, proximoItem) => intemAnterior + proximoItem )
+
+    return totalPrice * .05
+  }
+};
