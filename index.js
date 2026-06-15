ecab3afb1fe7333fc6f2718f5810f2186d94534e
iliberato dev <isaque.cedesp@gmail.com>
2026-06-15 14:49:28 -0300

    Add test 2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = function () {
-    return 0;
+module.exports = function (items) {
+    
+    if (!items.length) return 0;
+
+    const { precoUnitario, qtdVendida } = items[0]
+
+    return precoUnitario * qtdVendida * .05
 }
