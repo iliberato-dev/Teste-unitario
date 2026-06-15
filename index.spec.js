96dc3696995b6f19b16c9d56511b60a9adea75f1
iliberato dev <isaque.cedesp@gmail.com>
2026-06-15 14:38:07 -0300

    Add another fail test

--- index.spec.js
+++ index.spec.js
@@ -6,5 +6,19 @@ it('Se a lista estiver vazia, a comissão é zero', () => {
     const resultadoEsperado = 0;
 
     expect(resultadoAtual).toBe(resultadoEsperado);
+});
+
+it('calcula comissão quando só tem um item na lista', () => {
+
+    const resultadoAtual = calculaComissaoVenda([{
+        id: 'PROD-123',
+        precoUnitario: 1000,
+        qtdVendida: 1
+    }])
+
+    const resultadoEsperado = 50
+
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
 })
 
